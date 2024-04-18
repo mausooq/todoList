6a1885f10b4d52453437f23b205512f73184c280
mausooq <mauchoomausooq051@gmail.com>
2024-04-18 22:18:29 +0530

    new

--- Client/src/Create.jsx
+++ Client/src/Create.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 
 function Create() {
+  const [ task,setTask] = useState()
+  const handelAdd = () =>{
+    
+  }
   return (
     <div className='create_form'>
-        <input type="text" name="" id="" />
-        <button type="button">Add</button>
+        <input type="text" name="" id="" onChange={(e)=> setTask(e.target.value)} placeholder='enter Task'/>
+        <button type="button" onClick={addTask}>Add</button>
     </div>
   )
 }
--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+
+
 app.listen(8080,() => {
     console.log("server running at 8080");
 })
